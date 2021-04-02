20425712b5fa7451cb04ea49dd992a04ba9d2f82
Nevin Herring <nevin.herring@gmail.com>
2021-04-02 09:51:37 -0400

    added update method for todo

--- todo-it.ts
+++ todo-it.ts
@@ -1,1 +1,44 @@
 console.log("TodoIt");
+const todoList: string[] = [];
+const todoInput: HTMLInputElement = document.getElementById('todoInput') as HTMLInputElement;
+const todoListDiv: HTMLDivElement = document.getElementById('todoListContainer') as HTMLDivElement;
+
+function addTodo(): void {
+    // if we don't have the todo input
+    if(todoInput == null) {
+        console.error('The todo input is missing fro the page!');
+        return;
+    }
+
+    //get the value from the input
+    const newTodo: string = todoInput.value;
+
+    // verify that there is text
+    if('' !== newTodo.trim()){
+        console.log('Adding todo: ', newTodo);
+
+        // add the new item to the list
+        todoList.push(newTodo);
+        console.log('New todo list: ', todoList);
+
+        // clear the input
+        todoInput.value = '';
+    }
+}
+
+function updateTodoList(): void {
+    console.log("Updating the rendered todo list");
+    todoListDiv.innerHTML = '';
+    todoListDiv.textContent = ''; // Edge, ...
+
+    const ul = document.createElement('ul');
+    ul.setAttribute('id', 'todoList');
+    todoListDiv.appendChild(ul);
+
+    todoList.forEach(items => {
+        const li = document.createElement('li');
+        li.setAttribute('class', 'todo-list-item');
+        li.innerText = item;
+        ul.appendChild(li);
+    });
+}
